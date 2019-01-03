8084cc1631251bbcddf93618d10f68c4868c9d39
Riroaki <2569126376@qq.com>
2019-01-04 01:38:33 +0800

    Update client: ip and port can be changed.

--- src/client/Client.java
+++ src/client/Client.java
@@ -6,10 +6,9 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Client implements Interact {
-    private static final String IP_ADDR = "localhost";
-    private static final int PORT = 12409;
     private Socket socket;
     private DataOutputStream output;
     private DataInputStream input;
@@ -17,31 +16,55 @@ public class Client implements Interact {
     public static void main(String[] args) {
         System.out.println("Start client...");
         Client client = new Client();
-        client.init();
+        if (client.init() != 0)
+            System.exit(1);
         client.run();
     }
 
-    private void init() {
-        int reconnectTimes = 0;
+    private int init() {
         final int MAX_TRIAL = 10;
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Please input IP and PORT of the server:\n" +
+                "default is `localhost:12409`, type `default` to use it;\n" +
+                "or type `quit` to exit program.\n" +
+                " >>");
+
         while (true) {
-            try {
-                socket = new Socket(IP_ADDR, PORT);
-                // Read data from server.
-                input = new DataInputStream(socket.getInputStream());
-                // Send data to server.
-                output = new DataOutputStream(socket.getOutputStream());
-                break;
-            } catch (Exception e) {
-                if (reconnectTimes++ == MAX_TRIAL)
-                    die(1);
-                System.out.println("Fail to connect server, retry in 2 sec... ");
-                try {
-                    Thread.sleep(2000);
-                } catch (InterruptedException ie) {
-                    e.printStackTrace();
+            // Default IP and port.
+            int PORT = 12409;
+            String IP_ADDR = "localhost";
+            if (s.hasNextLine()) {
+                String[] addr = s.nextLine().split(":");
+                if ("quit".equals(addr[0]))
+                    return 1;
+                else if(!"default".equals(addr[0])) {
+                    try {
+                        IP_ADDR = addr[0];
+                        PORT = Integer.parseInt(addr[1]);
+                    } catch (Exception e) {
+                        System.out.println("Invalid input.");
+                    }
                 }
             }
+            for (int i = 1; i <= MAX_TRIAL; i++)
+                try {
+                    socket = new Socket(IP_ADDR, PORT);
+                    // Read data from server.
+                    input = new DataInputStream(socket.getInputStream());
+                    // Send data to server.
+                    output = new DataOutputStream(socket.getOutputStream());
+                    return 0;
+                } catch (Exception e) {
+                    if (i == MAX_TRIAL)
+                        die(1);
+                    System.out.println("Fail to connect server, retry in 2 sec... ");
+                    try {
+                        Thread.sleep(2000);
+                    } catch (InterruptedException ie) {
+                        e.printStackTrace();
+                    }
+                }
         }
     }
 
